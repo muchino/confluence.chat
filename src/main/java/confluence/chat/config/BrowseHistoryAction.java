8eac454b205b7c1d59951a4c57f39356d7d7ecd0
Oliver Sträßer <oliver.straesser@bitvoodoo.ch>
2014-07-18 18:27:23 +0200

    add debug output #91

--- src/main/java/confluence/chat/config/BrowseHistoryAction.java
+++ src/main/java/confluence/chat/config/BrowseHistoryAction.java
@@ -24,60 +24,66 @@ import net.sf.hibernate.HibernateException;
  */
 public class BrowseHistoryAction extends SearchUsersAction {
 
-    private PaginationSupport paginationSupportNew;
-    private PluginHibernateSessionFactory pluginHibernateSessionFactory;
+	private PaginationSupport paginationSupportNew;
+	private PluginHibernateSessionFactory pluginHibernateSessionFactory;
 
-    /**
-     * @param sessionFactory the sessionFactory to set
-     */
-    public void setPluginHibernateSessionFactory(PluginHibernateSessionFactory sessionFactory) {
-        this.pluginHibernateSessionFactory = sessionFactory;
-    }
+	/**
+	 * @param sessionFactory the sessionFactory to set
+	 */
+	public void setPluginHibernateSessionFactory(PluginHibernateSessionFactory sessionFactory) {
+		this.pluginHibernateSessionFactory = sessionFactory;
+	}
 
-    @Override
-    public String doUserSearch() {
-        super.doUserSearch();
-        List<String> allkeysWithBoxes = getAllKeysWithChatBoxes();
-        List chatResults = new ArrayList();
-        List results = super.getPaginationSupport().getItems();
-        for (int i = 0; i < results.size(); i++) {
-            Object object = results.get(i);
-            if (object instanceof User) {
-                String userKey = ChatUtils.getCorrectUserKey(((User) object).getName());
-                if (allkeysWithBoxes.contains(userKey)) {
-                    chatResults.add(object);
-                }
-            }
-        }
-        paginationSupportNew = new PaginationSupport(chatResults, 10);
-        paginationSupportNew.setStartIndex(getStartIndex());
-        return SUCCESS;
-    }
+	@Override
+	public String doUserSearch() {
+		super.doUserSearch();
+		List<String> allkeysWithBoxes = getAllKeysWithChatBoxes();
+		if (allkeysWithBoxes.isEmpty()) {
+			addActionMessage("There are no chats stored in the systen yet");
+		}
+		List chatResults = new ArrayList();
+		List results = super.getPaginationSupport().getItems();
+		for (Object object : results) {
+			if (object instanceof User) {
+				String userKey = ChatUtils.getCorrectUserKey(((User) object).getName());
+				if (allkeysWithBoxes.contains(userKey)) {
+					chatResults.add(object);
+				}
+			}
+		}
+		paginationSupportNew = new PaginationSupport(chatResults, 10);
+		paginationSupportNew.setStartIndex(getStartIndex());
+		return SUCCESS;
+	}
 
-    private List<String> getAllKeysWithChatBoxes() {
-        List<String> usernames = new ArrayList<String>();
-        Statement ps = null;
-        try {
-            ps = pluginHibernateSessionFactory.getSession().connection().createStatement();
-            ResultSet rs = ps.executeQuery(ChatManager.QUERY_HISTORIES);
-            while (rs.next()) {
-                String key = rs.getString(1);
-                String replace = key.replace(ChatManager.KEY_HISTORY, "");
-                usernames.add(replace);
-            }
-        } catch (HibernateException ex) {
-        } catch (SQLException ex) {
-        } finally {
-            JDBCUtils.close(ps);
-        }
-        return usernames;
-    }
+	private List<String> getAllKeysWithChatBoxes() {
+		List<String> usernames = new ArrayList<String>();
+		Statement ps = null;
+		try {
+			ps = pluginHibernateSessionFactory.getSession().connection().createStatement();
+			ResultSet rs = ps.executeQuery(ChatManager.QUERY_HISTORIES);
+			while (rs.next()) {
+				String key = rs.getString(1);
+				String replace = key.replace(ChatManager.KEY_HISTORY, "");
+				usernames.add(replace);
+			}
+		} catch (HibernateException ex) {
+			addActionError("Error while Hibernate execution: " + ex.getMessage());
+			LOG.error("Error while Hibernate execution: ", ex);
+		} catch (SQLException ex) {
+			addActionError("Error while SQ execution: " + ex.getMessage());
+			LOG.error("Error while SQ execution: ", ex);
+		} finally {
+			JDBCUtils.close(ps);
+		}
+		return usernames;
+	}
 
-    /**
-     * @return the paginationSupportNew
-     */
-    @Override
-    public PaginationSupport getPaginationSupport() {
-        return paginationSupportNew;
-    }
+	/**
+	 * @return the paginationSupportNew
+	 */
+	@Override
+	public PaginationSupport getPaginationSupport() {
+		return paginationSupportNew;
+	}
 }
