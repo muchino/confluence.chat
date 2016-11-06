88f47150cdba27dd4acaddaf8fd13498f640fc17
Oliver Sträßer <o.straesser.atlassian@gmail.com>
2016-11-06 12:30:01 +0100

    catch if notification is not available

--- src/main/resources/confluence/chat/js/chat-notification.js
+++ src/main/resources/confluence/chat/js/chat-notification.js
@@ -6,7 +6,6 @@
 // request permission on page load
     document.addEventListener('DOMContentLoaded', function () {
 	if (!Notification) {
-	    alert('Desktop notifications not available in your browser. Try Chromium.');
 	    return;
 	}
 
@@ -29,6 +28,7 @@
 	    };
 	}
     }
-
-    $.jStorage.subscribe(CHAT_NEW_MESSAGE, showMessage);
+    if (Notification) {
+	$.jStorage.subscribe(CHAT_NEW_MESSAGE, showMessage);
+    }
 })(AJS.$);
